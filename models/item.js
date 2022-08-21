860a95c4d6d86dfa38b3a6bb4c519968d1904f80
Elliot <elliotcs30@gmail.com>
2022-08-21 14:14:47 +0800

    feat: add mongoose

---
+++ models/item.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const itemSchema = new Schema({
+  // 資料型別是字串, 這是個必填欄位
+  Investors: { type: String, required: true },   // 出資人
+  item: { type: String, required: true},         // 項目
+  amount: { type: Number, required: true },      // 金錢
+  paymentUser: { type: String, required: true},  // 付款
+  settleAccount: { type: String, required: true} // 結算
+})
+
+module.exports = mongoose.model('Accounting', itemSchema)
