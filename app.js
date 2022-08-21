6eecffbe5bf8d699aaee893610b57ed8854e1a90
Elliot <elliotcs30@gmail.com>
2022-08-21 16:12:11 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express') // 載入 express 並建構應用程式伺服器
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
+const Item = require('./models/item') // 載入 Item model
 
 const app = express()
 
@@ -22,7 +23,10 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Item.find() // 取出 Item model 裡的所有資料
+    .lean()   // 把 Mongoose 的 Model 物件轉換成乾淨的 JS 資料陣列
+    .then(items => res.render('index', { items })) // 將資料傳給 index 樣板
+    .catch(err => console.log(err)) // 錯誤處理
 })
 
 // 設定 port 3000
